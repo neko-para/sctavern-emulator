c072c5dbfabaa8054c9ea689751d3b8ad70752f9
nekosu <liao.hy@outlook.com>
2022-11-22 18:59:47 +0800

    Fix base

--- packages/web-client/vite.config.ts
+++ packages/web-client/vite.config.ts
@@ -8,6 +8,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sctavern-emulator/',
   plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
