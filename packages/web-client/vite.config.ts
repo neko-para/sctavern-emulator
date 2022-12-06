9b1c6cf40cd182917f3fe17cd64de01d8bc38d9c
xm <bcis_xm@outlook.com>
2022-12-06 15:26:40 +0800

    [chore] optimze build config.

--- packages/web-client/vite.config.ts
+++ packages/web-client/vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, searchForWorkspaceRoot } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
@@ -13,6 +13,17 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    fs: {
+      allow: [
+        // 搜索工作区的根目录
+        searchForWorkspaceRoot(process.cwd()),
+        // 自定义规则
+        // 由于将依赖都装到了根目录的node_modules中，需要手动指定允许devServer访问根目录的node_modules
+        '../../node_modules',
+      ],
     },
   },
 })
