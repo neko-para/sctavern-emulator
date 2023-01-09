bc9e7dc666c5477b65bc11e8be9adba53526d8af
nekosu <liao.hy@outlook.com>
2023-01-09 23:10:28 +0800

    [feat] Add LCG

--- packages/framework/src/utils.ts
+++ packages/framework/src/utils.ts
@@ -22,6 +22,37 @@ export function Decompress<T>(save: string): T | null {
 
 export function Md5(data: string): string {
   return md5(data, {
-    asString: true
+    asString: true,
   })
+}
+
+export class LCG {
+  seed: number
+
+  constructor(begin: number) {
+    this.seed = begin
+  }
+
+  next() {
+    this.seed = (25214903917 * this.seed) & ((1 << 48) - 1)
+    return this.seed
+  }
+
+  nextReal() {
+    // [0, 1)
+    return this.next() / (1 << 48)
+  }
+
+  int(max: number, min = 0) {
+    // [min, max]
+    return min + this.nextReal() * (max - min + 1)
+  }
+
+  shuffle<T>(array: T[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = this.int(0, i)
+      ;[array[i], array[j]] = [array[j], array[i]]
+    }
+    return array
+  }
 }
